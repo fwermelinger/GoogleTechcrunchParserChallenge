5f4b25972f8cfabfab82675b7702ed5aee9067cb
Florian Wermelinger <f.wermelinger@gmail.com>
2015-11-29 14:09:58 +0700

    added missing files to source control

---
+++ Core/BaseParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebParserChallenge.Core
+{
+    public class BaseParser
+    {
+        private string RawHtml;
+        public HtmlAgilityPack.HtmlDocument htmlDocument { get; set; }
+
+        public BaseParser(string html)
+        {
+            if (string.IsNullOrEmpty(html))
+            {
+                throw new ArgumentException("html can not be empty");
+            }
+
+            this.RawHtml = html;
+
+            this.htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            this.htmlDocument.LoadHtml(this.RawHtml);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebParserChallenge
+{
+    class Program
+    {
+        private static bool USE_TESTDATA = true;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write("Argument 1 (search query) required");
+                return;
+            }
+
+            DoGoogleTask(args.First());
+            
+
+
+            DoTechCrunchTask();
+           
+
+            Console.ReadKey();
+        }
+
+        private static void DoTechCrunchTask()
+        {
+            Console.WriteLine("Getting TechCrunch Results:");
+            Console.WriteLine("===========================");
+
+            string resultHtml = "";
+
+            if (USE_TESTDATA)
+            {
+                resultHtml = System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "\\..\\..\\Testdata\\techcrunchresult.txt");
+            }
+            else
+            {
+                var uri = "http://techcrunch.com/";
+                var resultPage = new Core.WebPageDownloader(uri);
+
+                resultPage.DownloadPageAsync().Wait();
+                resultHtml = resultPage.Result;
+            }
+
+            var parser = new Core.TechCrunchParser(resultHtml);
+            var resultList = parser.GetResults();
+
+            foreach (var res in resultList.OrderBy(x => x.Title))
+            {
+                Console.WriteLine("{0} (by {1})\n{2}\n{3}\n", res.Title, res.Author, res.Excerpt, res.Url);
+            }
+
+            Console.WriteLine("=== End of TechCrungh Results ===" + Environment.NewLine);
+        }
+
+        private static void DoGoogleTask(string searchterm)
+        {
+            Console.WriteLine("Getting Google Results:");
+            Console.WriteLine("=======================");
+
+            string resultHtml = "";
+
+            if (USE_TESTDATA)
+            {
+                resultHtml = System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "\\..\\..\\Testdata\\googleresult.txt");
+            }
+            else
+            {                
+                var uri = string.Format("https://www.google.com/search?q={0}", System.Web.HttpUtility.UrlEncode(searchterm));
+                var searchResultPage = new Core.WebPageDownloader(uri);
+
+                searchResultPage.DownloadPageAsync().Wait();
+                resultHtml = searchResultPage.Result;
+            }
+
+            var googleParser = new Core.GoogleResultsParser(resultHtml);
+            var googleResults = googleParser.GetResults();
+
+            foreach (var res in googleResults.OrderBy(x => x.Title))
+            {
+                Console.WriteLine("{0}\n{1}\n", res.Title, res.Url);
+            }
+
+            Console.WriteLine("=== End of Google Results ===" + Environment.NewLine);
+        }
+
+        
+    }
+}
